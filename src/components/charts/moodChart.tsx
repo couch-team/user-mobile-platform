43c36e959061a0ce09916c8dcdd6f93356bba165
johnoris <johnorisanwo@gmail.com>
2024-03-13 08:54:01 +0100

    more fixes

--- src/components/charts/moodChart.tsx
+++ src/components/charts/moodChart.tsx
@@ -4,31 +4,8 @@ import { BarChart, LineChart } from "react-native-gifted-charts";
 import { $api } from "services";
 import { Colors } from "theme/config";
         
-const MoodChart = () => {
-    const [ is_loading, setIsLoading ] = useState(false);
-    const [ data, setData ] = useState<{ date: string, count: number }[]>([])
+const MoodChart = ({ data, is_loading } : { is_loading: boolean, data: { count: number, date: string }[] }) => {
     const { width } = Dimensions.get('window');
-
-    const fetchChartData = async() => {
-        try{
-            setIsLoading(true)
-            const response = await $api.fetch('/api/mood/dates')
-            if($api.isSuccessful(response)){
-                setData(response?.data)
-            }
-        }
-        catch(err){
-            console.log(err)
-        }
-        finally{
-            setIsLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        fetchChartData()
-    },[])
-
     const dataToRender = data?.map((chartData) => ({value:  chartData.count, dataPointText: chartData?.count?.toString(), label: chartData?.date?.split("-")[2] + '/' + chartData?.date?.split("-")[1] }))
     return ( data?.length === 0 && !is_loading ) ? <View></View> : (
         <View style={{ backgroundColor: '#FFFFFF0D', minHeight: 150, justifyContent: 'center', alignItems: 'center', paddingVertical: 24, borderRadius: 16, marginBottom: 32 }}>
