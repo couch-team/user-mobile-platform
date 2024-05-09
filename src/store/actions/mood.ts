ae21956a5dcb5054282e208647673af58d3c1dcc
Abdul-Qudus-09 <rufixduke09@gmail.com>
2024-05-09 13:26:55 +0100

    No changes made

--- src/store/actions/mood.ts
+++ src/store/actions/mood.ts
@@ -1,44 +1,54 @@
-import debounce from "helpers/debounce"
-import { $api } from "services"
-import { AppThunk } from "store"
-import { setChartData, setChartDataLoading, setHasFetchedMoods, setIsFetchingMoods, setMoods, setMoodsCurrentPage, setMoodsLastPage, setReachedEnd } from "store/slice/moodSlice"
+import debounce from 'helpers/debounce';
+import { $api } from 'services';
+import { AppThunk } from 'store';
+import {
+  setChartData,
+  setChartDataLoading,
+  setHasFetchedMoods,
+  setIsFetchingMoods,
+  setMoods,
+  setMoodsCurrentPage,
+  setMoodsLastPage,
+  setReachedEnd,
+} from 'store/slice/moodSlice';
 
-const debounceFetchMoods = debounce(async(current_page, dispatch) => {
-    try{
-        const response = await $api.fetch(`/api/mood/log/?page=${current_page}`)
-        if($api.isSuccessful(response)){
-            response?.data?.results?.length > 0 ? dispatch(setMoods(response?.data?.results)) : dispatch(setReachedEnd(true))
-            dispatch(setMoodsLastPage(response?.data?.count /15))
-        }
-        else{
-            response?.response?.status == 404 && dispatch(setReachedEnd(true))
-        }
+const debounceFetchMoods = debounce(async (current_page, dispatch) => {
+  try {
+    const response = await $api.fetch(`/api/mood/log/?page=${current_page}`);
+    if ($api.isSuccessful(response)) {
+      response?.data?.results?.length > 0
+        ? dispatch(setMoods(response?.data?.results))
+        : dispatch(setReachedEnd(true));
+      dispatch(setMoodsLastPage(response?.data?.count / 15));
+    } else {
+      response?.response?.status == 404 && dispatch(setReachedEnd(true));
     }
-    catch(err){
-        console.log(err)
-    }
-    finally{
-        dispatch(setIsFetchingMoods(false))
-    }
-}, 1000)
+  } catch (err) {
+    console.log(err);
+  } finally {
+    dispatch(setIsFetchingMoods(false));
+  }
+}, 1000);
 
-export const fetchMoods = (current_page: number, isSilent?: boolean):AppThunk => async(dispatch) => {
-    dispatch(setMoodsCurrentPage(current_page))
-    dispatch(setHasFetchedMoods(true))
-    !isSilent && dispatch(setIsFetchingMoods(true))
-    debounceFetchMoods(current_page, dispatch)
-}
-
-export const fetchChartData = ():AppThunk => async(dispatch) => {
-    try {
-      dispatch(setChartDataLoading(true));
-      const response = await $api.fetch('/api/mood/stats');
-      if ($api.isSuccessful(response)) {
-        dispatch(setChartData(response?.data))
-      }
-    } catch (err) {
-      console.log(err);
-    } finally {
-      dispatch(setChartDataLoading(false))
-    }
+export const fetchMoods =
+  (current_page: number, isSilent?: boolean): AppThunk =>
+  async dispatch => {
+    dispatch(setMoodsCurrentPage(current_page));
+    dispatch(setHasFetchedMoods(true));
+    !isSilent && dispatch(setIsFetchingMoods(true));
+    debounceFetchMoods(current_page, dispatch);
   };
+
+export const fetchChartData = (): AppThunk => async dispatch => {
+  try {
+    dispatch(setChartDataLoading(true));
+    const response = await $api.fetch('/api/mood/stats');
+    if ($api.isSuccessful(response)) {
+      dispatch(setChartData(response?.data));
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    dispatch(setChartDataLoading(false));
+  }
+};
